2086ca8684f19c9d4d5c02f3aa10abd311df165a
Anissachan <45741414+Anissachan@users.noreply.github.com>
2019-11-10 18:26:39 -0600

    updated Deck.cpp

--- deck.cpp
+++ deck.cpp
@@ -1,17 +1,51 @@
 //
 // Created by aniss on 11/9/2019.
 //
-
+#include <iostream>
+#include <ctime>
 #include "deck.h"
+#include "card.h"
+#include "player.h"
+
+using namespace std;
 
 Deck::Deck() {
-
+    unsigned int currentTime = (unsigned)time(0);
+    srand(currentTime);
+    //int _rank;
+    //string suit;
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 52; j++){
+            Card(j, i);
+        }
+    }
 }
 
 void Deck::shuffle() {
 
+    for(int i = 0; i < 52; i++) {
+        int index1 = (rand() % 13) + 1;
+        int index2 = (rand() % 13) + 1;
+
+        Card tempCard = myCards[index1];
+        myCards[index1] = myCards[index2];
+        myCards[index2] = tempCard;
+
+    }
+}
+
+void Deck::dealHand(Deck &d, Player &p, int numCards)
+{
+    for (int i=0; i < numCards; i++)
+        p.addCard(d.dealCard());
 }
 
 Card Deck::dealCard() {
+    while(myIndex <= 52)
+    myIndex++;
+}
 
+int Deck::size() const {
+    Player handCard;
+    int handSize = handCard.getHandSize();
 }
